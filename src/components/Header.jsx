39b37d6eca80d7192dc92e500f4c2307773f9d5c
Flavio Wuensche <flavio.wuensche@doctolib.com>
2024-11-09 09:21:33 +0100

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import Logo from '../assets/suricate.png'
+
 const Header = () => {
   return (
-    <div className='text-center mb-8'>
-      <h1 className='h1'>SURICATE</h1>
+    <div className='mb-8 flex items-center'>
+      <a href={Logo} target='_blank' rel='noopener noreferrer'>
+        <img src={Logo} alt='Suricate logo' className='w-20 h-20 mr-4 rounded-full' />
+      </a>
+      <h1>SURICATE</h1>
     </div>
   )
 }
