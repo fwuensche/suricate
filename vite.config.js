fec270b3ac75b3911ceca08a195872a4befe258b
Jérémi ferre <jeremi@MacBook-Pro-de-Jeremi.local>
2025-07-25 14:44:47 +0200

    modify build for offline compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/suricate/',
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    viteSingleFile()
+  ],
+  build: {
+    assetsInlineLimit: 100000000, // large number to inline all assets
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        manualChunks: undefined
+      }
+    }
+  }
 })
