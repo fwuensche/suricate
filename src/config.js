ab737a953bf0ca9d2d402f1715e6149d6f91a9c3
Flavio Wuensche <f.wuensche@gmail.com>
2025-10-15 09:07:45 +0200

    update menu

--- src/config.js
+++ src/config.js
@@ -20,8 +20,8 @@ export const MENU_ITEMS = [
     name: 'Suprême poulet',
     price: 10.5,
     ingredients: [
-      { name: 'Sauce Suricate', checked: true },
-      { name: 'Poulet pané', checked: true },
+      { name: 'Sauce Curry', checked: true },
+      { name: 'Filet de poulet', checked: true },
       { name: 'Oignons confits', checked: true },
       { name: 'Saint Nectaire', checked: true },
       { name: 'Mesclun', checked: true },
@@ -33,21 +33,22 @@ export const MENU_ITEMS = [
     price: 10.5,
     ingredients: [
       { name: 'Sauce Suricate', checked: true },
-      { name: 'Haché de bœuf', checked: true },
+      { name: 'Haché de Boeuf', checked: true },
       { name: 'Poitrine fumée', checked: true },
+      { name: 'PDT', checked: true },
       { name: 'Reblochon', checked: true },
       { name: 'Mesclun', checked: true },
     ],
   },
-  { section: 'Frites Maison', name: 'Frites maison', price: 3.5 },
-  { section: 'Boissons', name: 'Eau plate', price: 1.5 },
-  { section: 'Boissons', name: 'Perrier', price: 1.5 },
+  { section: 'Accompagnements', name: 'Frites Fraîches', price: 3.5 },
+  { section: 'Boissons', name: 'Cristaline', price: 1.5 },
+  { section: 'Boissons', name: 'Eau Perrier', price: 1.5 },
   { section: 'Boissons', name: 'Coca Cola', price: 2 },
   { section: 'Boissons', name: 'Coca Zéro', price: 2 },
   { section: 'Boissons', name: 'Ice tea', price: 2 },
   { section: 'Boissons', name: 'Oasis', price: 2 },
-  { section: 'Dessert Maison', name: 'Cookie Nutella', price: 3.5 },
-  { section: 'Dessert Maison', name: 'Café', price: 1.5 },
+  { section: 'Desserts', name: 'Cookie Nutella', price: 3.5 },
+  { section: 'Café', name: 'Café', price: 1.5 },
 ]
 
 const tooLongLabels = MENU_ITEMS.flatMap((item) => {
@@ -74,6 +75,6 @@ export const FORMULAS = [
   {
     name: 'Formule : Burger – Frites – Boisson',
     price: 14.5,
-    sections: ['Burgers', 'Frites Maison', 'Boissons'],
+    sections: ['Burgers', 'Accompagnements', 'Boissons'],
   },
 ]
