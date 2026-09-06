132832497789ed3a6df581546345c21e0f766eb0
Bruno FERRE <brunoferre@air-de-bruno.home>
2026-09-06 15:22:22 +0200

    update

--- src/config.js
+++ src/config.js
@@ -92,7 +92,7 @@ export const MENU_ITEMS = [
   { section: 'Desserts', name: 'Mousse choco', price: 2 },
   { section: 'Supplement', name: 'Sup. viande ', price: 3 },
   { section: 'Supplement', name: 'Sup. Ingredient ', price: 1 },
-  { section: 'Note de frais', name: '1 Repas complet', price: 16.5 },
+  { section: 'Note de frais', name: '1 Repas', price: 16.5 },
   { section: 'Note de frais', name: '1 Repas complet', price: 33 },
 ]
 
